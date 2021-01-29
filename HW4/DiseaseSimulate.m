8ef4c2cb1da602667a16f6f7def51944e7c4794f
Ryo Takei <takeiryou926@gmail.com>
2021-01-29 02:37:20 -0800

    First commit

---
+++ HW4/DiseaseSimulate.m
@@ -0,0 +1,4 @@
+function [SvsT, IvsT, RvsT, time] = DiseaseSimulate(SStart, IStart, a, b, h, nSteps)
+
+end
+
---
+++ HW4/DiseaseStep.m
@@ -0,0 +1,4 @@
+function [SOut, IOut, ROut] = DiseaseStep(SIn, IIn, RIn, N, a, b, h)
+
+end
+
